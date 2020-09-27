2c953db997c681b5ad13ea4f21d2f799a85e3338
Ahmed-Mohamed-Hussin <36509814+idrissmohamed99@users.noreply.github.com>
2020-09-27 10:55:57 +0200

    Add UseEndpoints in Configure

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -23,7 +23,6 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -32,7 +31,10 @@ namespace WishList
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
             app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+
         }
     }
 }
